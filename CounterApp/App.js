8a4ecaa9aea910776d899698cb8cb03af1978807
Mohammad Saud <mdsaud.mrj@gmail.com>
2023-08-22 20:17:21 +0530

    counter app : react-native

---
+++ CounterApp/App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter App</Text>
+      <Text style={styles.counter}>Count: {count}</Text>
+      <TouchableOpacity style={styles.button} onPress={incrementCount}>
+        <Text style={styles.buttonText}>Increment</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={decrementCount}>
+        <Text style={styles.buttonText}>Decrement</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  counter: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
